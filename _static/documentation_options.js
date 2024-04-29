61729236f50b41f8487fd0d7014f8a6ff9e8c5d8
KindXiaoming <KindXiaoming@users.noreply.github.com>
2024-04-29 20:06:56 +0000

    deploy: 4d03761329df31f6a18e93d8cadceca921626300

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,4 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
