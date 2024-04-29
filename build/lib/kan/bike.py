9c8f6239f80175f12e4878c79b4c5385688c906a
kindxiaoming <zmliu@mit.edu>
2024-04-29 00:54:00 -0400

    update

--- build/lib/kan/bike.py
+++
@@ -1,8 +0,0 @@
-class Bike:
-
-    def __init__(self, name: str):
-        self.name = name
-
-    def ride(self, road : str) -> bool:
-        print("Riding: %s on road: %s" % (self.name, road))
-        return True
