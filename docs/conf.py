0b11bf0b2bffe749479a42dfebcaaf9c38deaada
kindxiaoming <zmliu@mit.edu>
2024-04-29 14:28:23 -0400

    add mockimport

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,12 @@ def skip(app, what, name, obj, would_skip, options):
 def setup(app):
     app.connect("autodoc-skip-member", skip)
     
-autodoc_mock_imports = ["torch"
+autodoc_mock_imports = ["torch",
+                        "matplotlib",
+                        "numpy",
+                        "sklearn",
+                        "sympy",
+                        "tqdm"
                         ]
            
 
