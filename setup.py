2367bbecbb8f8b31d7ac5cc6c2dfdd8d228a2487
Ziming Liu <zmliu@mit.edu>
2024-05-09 06:36:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="pykan",
-    version="0.0.3",
+    version="0.0.4",
     author="Ziming Liu",
     author_email="zmliu@mit.edu",
     description="Kolmogorov Arnold Networks",
