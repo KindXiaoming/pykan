cc0daf4698696cb9ddcc599a0657d259ed490d1f
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-06-12 00:49:01 -0400

    update the installation setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     # url="https://github.com/kindxiaoming/",
     packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={  
+        'pykan': [
+            'figures/lock.png',
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
