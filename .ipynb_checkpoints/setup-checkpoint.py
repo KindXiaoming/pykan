c5ebd60476dbd526ac294b7e63e1fd7b3ef7d7ef
Ziming Liu <zmliu@mit.edu>
2024-11-13 13:38:26 -0500

    add __init__.py

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,31 @@
+import setuptools
+
+# Load the long_description from README.md
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pykan",
+    version="0.2.7",
+    author="Ziming Liu",
+    author_email="zmliu@mit.edu",
+    description="Kolmogorov Arnold Networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/kindxiaoming/",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={  
+        'pykan': [
+            'figures/lock.png',
+            'assets/img/sum_symbol.png',
+            'assets/img/mult_symbol.png',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
